0efd59d2633d0b3af9a6076542d0b372c9eab45e
William Li <billgan.lee@gmail.com>
2021-07-25 19:10:35 -0700

    test push first draw with turtle

--- Day 1.py
+++ Day 1.py
@@ -1,1 +1,17 @@
+import turtle as t
+
 print("Hello World.")
+
+t.pensize(7)
+t.color('red')
+t.forward(200)
+t.right(144)
+t.forward(200)
+t.right(144)
+t.forward(200)
+t.right(144)
+t.forward(200)
+t.right(144)
+t.forward(200)
+t.right(144)
+t.mainloop()
