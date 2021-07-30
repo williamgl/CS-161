47fca4c59848940a2e5cda4df7c05f73d0f3ce9a
William Li <billgan.lee@gmail.com>
2021-07-29 22:22:37 -0700

    day 2 - if-elif-else loop

---
+++ Day 2.py
@@ -0,0 +1,10 @@
+# Author: Gan Li
+# Date: 7/29/21 9:16 下午
+# Description: 添加了 Code Style
+number = float(input("Please input a number:"))
+if number > 0:
+    print("This is a positive number.")
+elif number < 0:
+    print("This is a negative number.")
+else:
+    print("The number is 0.")
